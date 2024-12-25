f11791437696ea70162cd0c2fa7286158df6735b
dipu59 <12biswasdipu@gmail.com>
2024-12-25 00:44:08 -0800

    Remove Function Added

--- script.js
+++ script.js
@@ -94,13 +94,36 @@ if(textInput.length <= 0){
     alert("Please enter a value");
     return;
 }
+else{
 
+    //p and li tag create>>>>>>>>>
     let list = document.createElement('li');
-    list.className= 'bg-white text-lg rounded-md w-[95%]  mx-auto my-3 py-2 px-4';
-    
-    list.innerHTML = textInput;
-    ul.appendChild(list);
+    let p = document.createElement('p');
+    list.className= 'bg-white text-lg rounded-md w-[95%]  mx-auto my-3 py-2 px-4 flex justify-between gap-2';
+    p.innerHTML = textInput;
+    ul.appendChild(list)
+    list.appendChild(p);
 
     inputField.value = '';
+ 
+    //creating edit button>>>>>>>.
+    const editBtn = document.createElement("button");
+    editBtn.innerText = 'Edit';
+    editBtn.className = 'bg-yellow-400 px-2 py-1 rounded-md font-semibold text-white'
+    list.appendChild(editBtn);
 
+    //creating remove button>>>>>.
+    const removeBtn = document.createElement("button");
+    removeBtn.innerText = 'Remove';
+    removeBtn.className = 'bg-red-600 px-2 py-1 rounded-md font-semibold text-white'
+    list.appendChild(removeBtn);
+
+}
+});
+
+//Function To update (Edit and Delete) button
+ul.addEventListener('click', (e)=>{
+if(e.target.innerHTML === 'Remove'){
+    ul.removeChild(e.target.parentElement);
+}
 })
